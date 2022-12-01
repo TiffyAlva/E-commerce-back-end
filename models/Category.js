d9e8cfc0575f40c5f1d808b2b6c29238967b22f9
TiffyAlva <TiffanyAlvarez54@gmail.com>
2022-12-01 17:55:08 -0500

    installed npm i

--- models/Category.js
+++ models/Category.js
@@ -1,3 +1,24 @@
+// const { Model, DataTypes } = require('sequelize');
+
+// const sequelize = require('../config/connection.js');
+
+// class Category extends Model {}
+
+// Category.init(
+//   {
+//     // define columns
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'category',
+//   }
+// );
+
+// module.exports = Category;
+
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
@@ -6,7 +27,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -1,3 +1,25 @@
+// // import models
+// const Product = require('./Product');
+// const Category = require('./Category');
+// const Tag = require('./Tag');
+// const ProductTag = require('./ProductTag');
+
+// // Products belongsTo Category
+
+// // Categories have many Products
+
+// // Products belongToMany Tags (through ProductTag)
+
+// // Tags belongToMany Products (through ProductTag)
+
+// module.exports = {
+//   Product,
+//   Category,
+//   Tag,
+//   ProductTag,
+// };
+
+
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
@@ -5,12 +27,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+}) 
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id"
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id"
+})
 
 module.exports = {
   Product,
